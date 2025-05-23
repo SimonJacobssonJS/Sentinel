b4038403081ab824644b91ffc9bc8b681bab712c
Simon Jacobsson <simon.jacobsson@chasacademy.se>
2025-05-23 09:28:23 +0200

    Debugging trying to fix the code

--- models/sensorData.js
+++ models/sensorData.js
@@ -1,28 +1,35 @@
 export default (sequelize, DataTypes) => {
-  const SensorData = sequelize.define('SensorData', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
+  const SensorData = sequelize.define(
+    'SensorData',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      device_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      timestamp: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      temperature: DataTypes.FLOAT,
+      humidity: DataTypes.INTEGER,
+      gas: DataTypes.INTEGER,
+      fall_detected: DataTypes.BOOLEAN,
+      heart_rate: DataTypes.INTEGER,
+      noise_level: DataTypes.INTEGER,
+      steps: DataTypes.INTEGER,
+      device_battery: DataTypes.INTEGER,
+      watch_battery: DataTypes.INTEGER,
     },
-    device_id: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    timestamp: {
-      type: DataTypes.DATE,
-      allowNull: false
-    },
-    temperature: DataTypes.FLOAT,
-    humidity: DataTypes.INTEGER,
-    gas: DataTypes.INTEGER, // från sensors.gas.ppm
-    fall_detected: DataTypes.BOOLEAN,
-    heart_rate: DataTypes.INTEGER,
-    noise_level: DataTypes.INTEGER,
-    steps: DataTypes.INTEGER,
-    device_battery: DataTypes.INTEGER,
-    watch_battery: DataTypes.INTEGER
-  });
+    {
+      tableName: 'SensorData',
+      timestamps: false, // or true, if you want createdAt/updatedAt
+    }
+  );
 
   return SensorData;
 };
